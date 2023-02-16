9ff39deb8444546f6650222e3d709cfa9bbf67ef
Charles DZADU <dzaducharles@gmail.com>
2023-02-16 04:23:26 -0800

    Last task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,13 @@ int main(void)
 	char c = 'C';
 	int it = 24;
 	long lit = 45;
+	unsigned long uit = 34;
 	float flt = 4.6;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(it));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(lit));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(lit));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(uit));
 	printf("Size of a float: %lu byte(s)\n", sizeof(flt));
 
 	return (0);
