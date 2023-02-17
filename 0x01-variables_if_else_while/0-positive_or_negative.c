4e56434f32a4614fa63d598deaa33bd2ef84f198
Charles DZADU <dzaducharles@gmail.com>
2023-02-17 02:56:44 -0800

    Last last fix for task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,7 +20,7 @@ int main(void)
 		printf("%d is negative\n", n);
 	} else if (n > 0)
 	{
-		printf("%d is positive \n", n);
+		printf("%d is positive\n", n);
 	} else
 	{
 		printf("%d is zero\n", n);
