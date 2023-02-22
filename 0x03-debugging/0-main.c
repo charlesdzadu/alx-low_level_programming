bfc736b8dd6203f74e135a495e83fd268aa59704
Charles DZADU <dzaducharles@gmail.com>
2023-02-22 03:30:18 -0800

    fix task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - Test positive or negative
- * test functions funtions that give correct output when giving 0
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
