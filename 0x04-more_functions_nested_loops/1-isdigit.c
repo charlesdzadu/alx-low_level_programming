b320b6d5c087335d1fa968a0900bf9a875476ccd
Charles DZADU <dzaducharles@gmail.com>
2023-02-23 04:25:13 -0800

    task to 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * isdigit - Check if param is digit
+ *
+ * @c: parameter
+ * Return: 1 if is a success 0 if it is error
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 9);
+}
