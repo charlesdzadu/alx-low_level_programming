e156e74633623cfc9ccb351ddb9e9cc5caf8bfc2
Charles DZADU <dkccharlie@gmail.com>
2023-04-03 09:18:18 +0000

    First task: Print list node

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_listint - Print a list in linked list
+ * @head:  Head of node
+ * Return: The number of total node
+ */
+
+size_t print_listint(const listint_t *head)
+{
+	int count = 0;
+
+	while (head != null)
+	{
+		count++;
+		printf("%d", head->data);
+		head = head->next;
+	}
+
+	return (count);
+
+}
+
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+# Writing more single linked list
