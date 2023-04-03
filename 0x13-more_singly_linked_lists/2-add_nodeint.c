c6c4b973649e7868fd08df2289de7748e95b4ef7
Charles DZADU <dkccharlie@gmail.com>
2023-04-03 19:15:13 +0000

    Correct Betty error

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,6 +4,7 @@
  * add_nodeint - Add a new node of int to be begining of nodes
  * @head: A pointer to the head of node
  * @n: New data (int) to add to node
+ * Return: A pointer to a new node created
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
