47ff278b268e4fae2fa6729ebb0cc086684673c4
Charles DZADU <dkccharlie@gmail.com>
2023-04-03 19:12:11 +0000

    Control new node created

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,17 +8,16 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *node = malloc(sizeof(listint_t));
 
-	if (new == NULL)
+	if (!node)
 	{
 		return (NULL);
 	}
 
-	new->n = n;
-	new->next = *head;
+	node->n = n;
+	node->next = *head;
+	*head = node;
 
-	*head = new;
-
-	return (new);
+	return (node);
 }
