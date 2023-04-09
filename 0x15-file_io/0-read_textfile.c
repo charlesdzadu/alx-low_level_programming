ab385bd836c18f621891fb224dd83fc770b9cb5e
Charles DZADU <dkccharlie@gmail.com>
2023-04-09 17:24:17 +0000

    Update read tex file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,8 +20,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	op = open(filename, O_RDONLY);
-	rd = read(o, buffer, letters);
-	wr = write(STDOUT_FILENO, buffer, r);
+	rd = read(o, buf, letters);
+	wr = write(STDOUT_FILENO, buf, r);
 
 	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
 	{
