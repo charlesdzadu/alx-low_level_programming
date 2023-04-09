934393c51172202a2dab52a82ad605ab4de00e1e
Charles DZADU <dkccharlie@gmail.com>
2023-04-09 17:39:06 +0000

    Update copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,10 +5,9 @@
  * Return: 0 if success and other interger if not
  */
 
-
 int main(int argc, char *argv[])
 {
-	int op_r, op_w, x, m, n;
+	int fd_r, fd_w, x, m, n;
 	char buf[BUFSIZ];
 
 	if (argc != 3)
@@ -16,19 +15,19 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	op_r = open(argv[1], O_RDONLY);
-	if (op_r < 0)
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	op_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((x = read(op_r, buf, BUFSIZ)) > 0)
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((x = read(fd_r, buf, BUFSIZ)) > 0)
 	{
-		if (op_w < 0 || write(op_w, buf, x) != x)
+		if (fd_w < 0 || write(fd_w, buf, x) != x)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(op_r);
+			close(fd_r);
 			exit(99);
 		}
 	}
@@ -37,14 +36,14 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	m = close(op_r);
-	n = close(op_w);
+	m = close(fd_r);
+	n = close(fd_w);
 	if (m < 0 || n < 0)
 	{
 		if (m < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", op_r);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
 		if (n < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", op_w);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
 	return (0);
