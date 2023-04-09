93c6e95c54673e22d87cf62cc3cf89bdd9d2c01f
Charles DZADU <dkccharlie@gmail.com>
2023-04-09 18:20:24 +0000

    Update copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
+char *create_buffer(char *file);
+void close_file(int fd);
+
 /**
- * create_buffer - Allocates bytes for a buffer.
- * @file: The filename
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
  *
- * Return: A pointer to a buffer
+ * Return: A pointer to the newly-allocated buffer.
  */
 char *create_buffer(char *file)
 {
@@ -23,26 +26,26 @@ char *create_buffer(char *file)
 }
 
 /**
- * close_file - Closes file .
- * @op: The file desc to be closed.
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
  */
-void close_file(int op)
+void close_file(int fd)
 {
 	int c;
 
-	c = close(op);
+	c = close(fd);
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", op);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
 
 /**
- * main - Copies the contents of a file to another 
- * @argc: The number of arguments
- * @argv: An array of pointers
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  */
