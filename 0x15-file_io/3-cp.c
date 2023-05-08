ce4dfc018a332b2feb27f4a76a64eac8e9116a3c
Charles DZADU <dkccharlie@gmail.com>
2023-05-08 17:36:45 +0000

    Remove close file function to different file and add it to main file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,20 @@
 #include "main.h"
 
+/**
+ * close_file - Close open file description
+ * @fd: File description
+ */
+
+void close_file(int fd)
+{
+	int c = close(fd);
+
+	if (c < 1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
+		exit(100);
+	}
+}
 
 /**
  * main - Copy text file to another
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,5 @@
 ssize_t read_textfile(char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void close_file(int file_description);
 
 #endif
